c058d6c90333d488da8011a4cde1e12f85b43901
helloheatherw <heather.williams@teachfirst.org.uk>
2020-10-02 19:33:51 +0100

    Add an icon from font awesome as an example of importing in to project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
+
+import "./Header.css";
 
 const useStyles = makeStyles(() => ({
     header: {
-      textAlign: "center"
+        textAlign: "center"
     },
 }));
 
 function Header() {
     const classes = useStyles();
     return (
-        <Typography className={classes.header} variant="h3">
-            To Do!
-        </Typography>
+        <div class="header">
+            <FontAwesomeIcon className="icon--calendar" icon={ faCalendarCheck }/>
+            <Typography className={classes.header} variant="h3">
+                To Do!
+            </Typography>
+        </div>         
     );
 }
 
