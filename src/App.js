e2d89a5a5cd7df27e69f58ef3a4048bc0835ea19
helloheatherw <heather.williams@teachfirst.org.uk>
2020-10-20 19:24:17 +0100

    Initial implementation of get and post requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
+
 import Header from './components/Header/Header';
 import TaskInput from './components/TaskInput/TaskInput';
 import TaskCounter from './components/TaskCounter/TaskCounter';
@@ -9,24 +11,40 @@ import TaskListing from './components/TaskListing/TaskListing';
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    { taskDescription: "Buy some milk", completed: false },
-    { taskDescription: "Walk the dog", completed: false},
-    { taskDescription: "Empty the dishwasher", completed: false},
-    { taskDescription: "Write some react", completed: false},
-    { taskDescription: "Remember hooks!!!!", completed: true},
-    { taskDescription: "Changing state", completed: false}
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    //A function to get the tasks
+    axios
+    .get("https://dxmg4z4kvl.execute-api.eu-west-2.amazonaws.com/dev/tasks?userId=47801de2-98b0-4bce-a7ed-a")
+    .then(
+      //if the request is successful
+      response => setTasks(response.data)
+    )
+    .catch(
+      //if the request returns an error
+      error => console.log(error))
+    
+  }, 
+  //How often should I run this function
+  []
+  );
 
   const addTask = text => {
     const newTask = {
+      completed: false,
+      userId: "47801de2-98b0-4bce-a7ed-a",
       taskDescription: text,
-      completed: false
     }
+    //Post request, pass it the new task
+    //if the post is successful, then add it to the app state
+    //if the post is not successful, log out the error
 
-    const updatedTasks = [ ...tasks, newTask];
-
-    setTasks(updatedTasks);
+    axios
+    .post("https://dxmg4z4kvl.execute-api.eu-west-2.amazonaws.com/dev/tasks", newTask)
+    .then(() => axios.get("https://dxmg4z4kvl.execute-api.eu-west-2.amazonaws.com/dev/tasks?userId=47801de2-98b0-4bce-a7ed-a"))
+    .then(response => setTasks(response.data))
+    .catch(error => console.log(error))
   }
   
   return (
